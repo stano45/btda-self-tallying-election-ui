7d794df36239cc8f9bfcc5732917d6669522d16a
s.kosorin <s.kosorin@campus.tu-berlin.de>
2024-07-03 12:57:41 +0200

    Add hooks, start voting flow

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { AppShell, MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
+import { ModalsProvider } from '@mantine/modals';
 import { theme } from '../theme';
 import { Web3Provider } from '@/contexts/Web3Context';
 import Header from '@/components/Header';
@@ -12,21 +13,23 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider theme={theme}>
       <Web3Provider>
-        <Notifications position="top-right" zIndex={1000} />
-        <Head>
-          <title>Election</title>
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-          />
-          <link rel="shortcut icon" href="/favicon.svg" />
-        </Head>
-        <AppShell header={{ height: 60 }} padding="md">
-          <Header />
-          <AppShell.Main>
-            <Component {...pageProps} />
-          </AppShell.Main>
-        </AppShell>
+        <ModalsProvider>
+          <Notifications position="top-right" zIndex={1000} />
+          <Head>
+            <title>Election</title>
+            <meta
+              name="viewport"
+              content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+            />
+            <link rel="shortcut icon" href="/favicon.svg" />
+          </Head>
+          <AppShell header={{ height: 60 }} padding="md">
+            <Header />
+            <AppShell.Main>
+              <Component {...pageProps} />
+            </AppShell.Main>
+          </AppShell>
+        </ModalsProvider>
       </Web3Provider>
     </MantineProvider>
   );
---
+++ types/types.ts
@@ -0,0 +1,4 @@
+export interface Candidate {
+  name: string;
+  id?: string;
+}
